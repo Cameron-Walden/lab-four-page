e77071e755ea4bab8013ac7ded61b69244d2d993
Cameron Walden <cameronkwalden@gmail.com>
2021-07-28 16:35:03 -0700

    added app1 and app2

---
+++ app1.js
@@ -0,0 +1,7 @@
+let userName = prompt("What's your name?")
+
+if(userName == "Cameron") {
+    prompt("Hello " + userName + "! How are you today?" );
+} else {
+    prompt("Hello, welcome to my site");
+}
