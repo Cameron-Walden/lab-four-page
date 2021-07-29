4eea1ebcc22fe599ac7461bff81af3d3bb601335
Cameron Walden <cameronkwalden@gmail.com>
2021-07-29 11:46:51 -0700

    updated code again

--- app2.js
+++ app2.js
@@ -1,7 +1,7 @@
 
 function howManyAlbums() {
     let depecheModeAlbums = prompt("How Many albums does Depeche Mode have")
-    if(depecheModeAlbums > 30 || depecheModeAlbums < 0) {
+    if(depecheModeAlbums > 21 || depecheModeAlbums < 0) {
         alert("Hmm...A quick google search should help you out.");
     } else {
         alert("There ya go...Come on in.");
