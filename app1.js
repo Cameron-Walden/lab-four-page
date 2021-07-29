3e541693b6ad9f7b1c04bbaab4b0046cec0ecd8c
Cameron Walden <cameronkwalden@gmail.com>
2021-07-29 12:17:08 -0700

    updated code again

--- app1.js
+++ app1.js
@@ -1,9 +1,9 @@
 function myUserPrompt() {
     let userPrompt = prompt("Do you like Depeche Mode? Yes or No.")
 
-    if( userPrompt == "Yes".toLowerCase()) {
+    if( userPrompt == "Yes" || userPrompt == "yes") {
     alert("Nice me too. One more question for you.");
-    } else if(userPrompt == "No".toLowerCase()) {
+    } else if(userPrompt == "No" || userPrompt == "no") {
     alert("Why are you visiting this site then?");
     } else {
     alert("Hey. Yes or no answers only...");
