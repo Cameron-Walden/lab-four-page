43dc1ac9f1c75335ed8cd30f74f4ed4668cf4bfe
Cameron Walden <cameronkwalden@gmail.com>
2021-07-29 11:57:56 -0700

    fixed grammatical error in prompt

--- app2.js
+++ app2.js
@@ -1,6 +1,6 @@
 
 function howManyAlbums() {
-    let depecheModeAlbums = prompt("How Many albums does Depeche Mode have")
+    let depecheModeAlbums = prompt("How Many albums does Depeche Mode have?")
     if(depecheModeAlbums > 21 || depecheModeAlbums < 0) {
         alert("Hmm...A quick google search should help you out.");
     } else {
